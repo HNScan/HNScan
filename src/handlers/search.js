2399ac47590a78b092aac80fe7dd240f03ffafc1
s-pk <seanpkilgarriff@gmail.com>
2018-12-07 18:50:18 -0600

    add search bar

--- src/handlers/search.js
+++ src/handlers/search.js
@@ -1,17 +1,51 @@
 const { getClient } = require("../util/clients.js");
+const { Address } = require("hsd");
+const rules = require("hsd/lib/covenants/rules");
 
 async function searchHandler(request, h) {
   const client = getClient();
+  let info = await client.getInfo();
+
+  let txHash = RegExp("^[a-fA-F0-9]{64}$");
+
   let search = request.query.q;
 
-  //TX hash = 64 length
+  if (!isNaN(search)) {
+    let height = +search;
 
-  //Address - regex for TS, SS, RS, HS
-  //Check what network is running and change regex search
+    let tip = info.chain.height;
 
-  //Block
+    //If it's a feasible block height, redirect to block page.
+    if (height <= tip) {
+      return h.response(`/block/${height}`).code(200);
+    }
 
-  //Name
+    return h.response().code(404);
+  }
+
+  if (txHash.test(search)) {
+    return h.response(`/tx/${search}`).code(200);
+  }
+
+  let address;
+
+  try {
+    address = new Address(search);
+  } catch (e) {
+    //Do nothing.
+  }
+
+  if (address) {
+    if (address.isValid()) {
+      return h.response(`/address/${search}`).code(200);
+    }
+  }
+
+  if (rules.verifyString(search)) {
+    return h.response(`/name/${search}`).code(200);
+  }
+
+  return h.response().code(404);
 }
 
 module.exports = searchHandler;
