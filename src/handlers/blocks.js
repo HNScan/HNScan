3799a8f7bd16b93124bf54a3f5769daa476d1c37
kilpatty <seanpkilgarriff@gmail.com>
2019-01-28 12:57:19 -0600

    transition blocks handler over to new api requests

--- src/handlers/blocks.js
+++ src/handlers/blocks.js
@@ -1,11 +1,11 @@
 const { getClient } = require("../util/clients.js");
 
+const { getBlocks } = require("../util/api.js");
+
 async function blocksHandler(request, h) {
   //XXX handle out of range pages -> Forward them to 404.
   let amount;
   let page;
-  // let start;
-  // let end;
 
   const client = getClient();
 
@@ -32,34 +32,16 @@ async function blocksHandler(request, h) {
     return h.response().code(404);
   }
 
-  let block;
-
   endBlock = currentBlock - amount;
 
   if (endBlock < -1) {
     endBlock = -1;
   }
 
-  let block1;
-  let block2;
-
-  while (currentBlock > endBlock) {
-    //Take another check at these options here.
-    //We want as little info as possible while still filling out all values.
-    try {
-      block = await client.execute("getblockbyheight", [
-        currentBlock,
-        true,
-        true
-      ]);
-      block.coinbaseTx = await client.getTX(block.tx[0].txid);
-    } catch (e) {
-      console.log(e);
-    }
-
-    blocks.push(block);
-
-    currentBlock--;
+  try {
+    blocks = await getBlocks(endBlock, currentBlock);
+  } catch (e) {
+    h.response().code(404);
   }
 
   return h.view("blocks.pug", {
--- src/util/api.js
+++ src/util/api.js
@@ -63,7 +63,40 @@ async function _getAddressHistoryDaemon(addressHash, page) {
   return data;
 }
 
+/**
+ * getAddressBalance
+ *
+ * @param addressHash
+ * @returns {async function}
+ */
 async function getAddressBalance(addressHash) {
+  assert(addressHash);
+
+  //Check if Urkel is enabled in the config.
+  if (config.has("urkel-url")) {
+    return _getAddressBalanceUrkel(addressHash);
+  } else {
+    return _getAddressBalanceDaemon(addressHash);
+  }
+}
+
+/**
+ * _getAddressBalanceUrkel
+ *
+ * @param addressHash
+ * @returns {balance}
+ */
+async function _getAddressBalanceUrkel(addressHash) {
+  return null;
+}
+
+/**
+ * _getAddressBalanceDaemon
+ *
+ * @param addressHash
+ * @returns {balance}
+ */
+async function _getAddressBalanceDaemon(addressHash) {
   const nomenclate = getNomenclate();
 
   //Declare functionwide vars
@@ -78,7 +111,44 @@ async function getAddressBalance(addressHash) {
   return balance;
 }
 
+async function getBlocks(from, to) {
+  assert(from < to);
+  assert(from >= -1);
+
+  let blocks = [];
+
+  //Flip so that blocks return in the correct order
+  for (let i = to; i > from; i--) {
+    let block = await getBlock(i);
+
+    blocks.push(block);
+  }
+
+  return blocks;
+}
+
+async function getBlock(height) {
+  if (config.has("urkel-url")) {
+    return _getBlockUrkel(height);
+  } else {
+    return _getBlockDaemon(height);
+  }
+}
+
+async function _getBlockDaemon(height) {
+  const client = getClient();
+
+  let block = await client.execute("getblockbyheight", [height, true, true]);
+  block.coinbaseTx = await client.getTX(block.tx[0].txid);
+
+  return block;
+}
+
+async function _getBlockUrkel(height) {}
+
 module.exports = {
   getAddressBalance: getAddressBalance,
-  getAddressHistory: getAddressHistory
+  getAddressHistory: getAddressHistory,
+  getBlocks: getBlocks,
+  getBlock: getBlock
 };
