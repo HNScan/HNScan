b63eb96efdeb264d6e9d9dd7d83b22346b9bb4a8
kilpatty <seanpkilgarriff@gmail.com>
2019-02-02 18:45:17 -0600

    move name handler to new format

--- src/handlers/names.js
+++ src/handlers/names.js
@@ -1,59 +1,24 @@
-const { getClient } = require("../util/clients.js");
+const { getNames } = require("../api");
+
+const { paginate } = require("../util/util.js");
 
 async function namesHandler(request, h) {
-  let amount;
-  let page;
-  const client = getClient();
+  let limit = request.query.limit;
+  let page = request.query.p;
+  let offset = (page - 1) * limit;
 
-  if (request.query.amt) {
-    amount = parseInt(request.query.amt);
-  } else {
-    amount = 25;
-  }
-
-  if (request.query.p) {
-    page = parseInt(request.query.p);
-  } else {
-    page = 1;
-  }
-
-  let offset = (page - 1) * amount;
-
-  let names;
+  let data;
   try {
-    names = await client.execute("getnames");
+    data = await getNames(limit, offset);
   } catch (e) {
     console.log(e);
   }
 
-  //Double check this works with all edgecases
-  if (offset > names.length) {
-    h.response().status(404);
-  }
-
-  //Sort the names by block height, and then alphabetical.
-  //XXX Probably break this out to util.
-  names.sort(function(a, b) {
-    if (b.height !== a.height) {
-      return b.height - a.height;
-    }
-    if (a.name === b.name) {
-      return 0;
-    }
-    return a.name > b.name ? 1 : -1;
-  });
-
-  let returnNames = names.slice(offset, offset + amount);
-  let totalPages = Math.ceil((names.length + 1) / amount);
+  let pagination = paginate(data.total, limit, page, "names");
 
   return h.view("names.pug", {
-    names: returnNames,
-    templateName: "names",
-    pagination: {
-      url: "names",
-      page,
-      totalPages
-    }
+    names: data.result,
+    pagination
   });
 }
 
