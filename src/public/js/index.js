6be757d32fa48e86b6192e066d17e4f2fecb386f
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2019-03-13 01:21:03 -0500

    responsive theme toggle button in the footer. This stores a checked=true for fark theme and checked=false for light theme and adjusts the slider accordingly. Also included in this PR is a fix to the navbar hidden items that kept showing through

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,3 +1,61 @@
+// ============== THEMES ==============
+// Check for theme in local storage and adjust toggle switch on page load
+(() => {
+  let checked = JSON.parse(localStorage.getItem("checked"));
+  if (checked == true) {
+    document.querySelector("#themeSwitch").checked = true;
+  } else {
+    document.querySelector("#themeSwitch").checked = false;
+  }
+})();
+
+function toggleTheme(e) {
+  document.documentElement.classList.add("color-theme-in-transition");
+
+  if (
+    localStorage.getItem("theme") === "light" &&
+    localStorage.getItem("checked") === "false"
+  ) {
+    localStorage.setItem("theme", "dark");
+    localStorage.setItem("checked", "true");
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+    localStorage.setItem("checked", "false");
+    document.documentElement.removeAttribute("data-theme", "dark");
+  }
+  setTimeout(() => {
+    document.documentElement.classList.remove("color-theme-in-transition");
+  }, 1000);
+}
+
+// ============== NAVBAR ==============
+// Navbar dropdown click event listener
+(() => {
+  let dropdowns = document.querySelectorAll(".navbarMore");
+  for (dropdown of dropdowns) {
+    dropdown.addEventListener("click", function(e) {
+      // Note, this will only work if navbarMore only has 1 sibling that is a navbar-dropdown
+      let sibling = e.target.nextSibling;
+      // sibling.classList.toggle("navbar-dropdown-visible");
+      sibling.classList.toggle("is-hidden-touch");
+    });
+  }
+})();
+
+function toggleMobileNav(e) {
+  const navbarBurger = document.querySelector(".navbar-burger");
+  const navbarMenu = document.querySelector(".navbar-menu");
+  const moreElement = document.querySelector(".navbarMore");
+
+  if (navbarBurger && navbarMenu && moreElement) {
+    navbarBurger.classList.toggle("is-active");
+    navbarMenu.classList.toggle("is-active");
+  }
+}
+
+// ============== SEARCH ==============
+// Handle searches by hitting the backend for blockchain information
 function search() {
   event.preventDefault();
   const searchbar = document.querySelector(".searchbar");
@@ -14,8 +72,7 @@ function search() {
     xhr.onreadystatechange = () => {
       if (xhr.readyState == 4 && xhr.status == 200) {
         window.location.href = xhr.responseText;
-      }
-      else if (xhr.readyState == 4 && xhr.status !== 200) {
+      } else if (xhr.readyState == 4 && xhr.status !== 200) {
         console.log("Error" + xhr.responseText);
       }
     };
@@ -23,44 +80,3 @@ function search() {
     xhr.send();
   }
 }
-
-function toggleMobileNav(e) {
-  const navbarBurger = document.querySelector(".navbar-burger");
-  const navbarMenu = document.querySelector(".navbar-menu");
-  const moreElement = document.querySelector(".navbarMore");
-
-  if (navbarBurger && navbarMenu && moreElement) {
-    navbarBurger.classList.toggle("is-active");
-    navbarMenu.classList.toggle("is-active");
-  }
-}
-
-function toggleTheme(e) {
-  e.preventDefault();
-  document.documentElement.classList.add('color-theme-in-transition');
-
-  if (localStorage.getItem('theme') === 'light') {
-    localStorage.setItem('theme', 'dark');
-    document.documentElement.setAttribute('data-theme', 'dark');
-  }
-  else {
-    localStorage.setItem('theme', 'light');
-    document.documentElement.removeAttribute('data-theme', 'dark');
-  }
-  setTimeout(() => {
-    document.documentElement.classList.remove('color-theme-in-transition')
-  }, 1000)
-}
-
-function navDropdownListeners() {
-  let dropdowns = document.querySelectorAll(".navbarMore");
-  for (dropdown of dropdowns) {
-    dropdown.addEventListener("click", function(e) {
-      // Note, this will only work if navbarMore only has 1 sibling that is a navbar-dropdown
-      let sibling = e.target.nextSibling;
-      sibling.classList.toggle("navbar-dropdown-visible");
-    });
-  }
-}
-
-navDropdownListeners();
