a88b953f0e58921142acfc670a3acdc4b21a0b08
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2019-08-23 18:38:47 -0500

    limiting the number of peers per page. Pagination not working yet.

--- src/handlers/tools/peers.js
+++ src/handlers/tools/peers.js
@@ -1,8 +1,31 @@
 const { getClient } = require("../../util/clients.js");
+const { paginate } = require("../../util/util.js");
 
 async function peersHandler(request, h) {
-  const client = getClient();
-  const peers = await client.execute("getpeerinfo");
+  let client;
+  let peersInfo;
+  let peers = [];
+  //TODO: this needs to change to 10/page when it is working
+  let limit = 3;
+  let page = request.query.p;
+  let offset = (page - 1) * limit;
+
+  try {
+    client = getClient();
+    peersInfo = await client.execute("getpeerinfo");
+  } catch (e) {
+    h.response().code(404);
+  }
+
+  for (let i = offset; i < offset + limit; i++) {
+    if (peersInfo[i]) {
+      peers.push(peersInfo[i]);
+    }
+  }
+
+  //TODO: The pagination is broken at the moment
+  let pagination = paginate(peers[0] + offset, limit, page, "peers");
+
   return h.view("peers.pug", { peers });
 }
 
