ebb31d6d198b9a981f6e5c78b54fcce67dea210d
JGeiser9 <32041734+JGeiser9@users.noreply.github.com>
2019-08-26 13:44:29 -0500

    starting fresh with React

--- src/api/index.js
+++
@@ -1,7 +0,0 @@
-module.exports.getAddress = require("./address.js");
-module.exports.getBlock = require("./block.js");
-module.exports.getBlocks = require("./blocks.js");
-module.exports.getName = require("./name.js");
-module.exports.getNames = require("./names.js");
-module.exports.getTX = require("./tx.js");
-module.exports.getTXs = require("./txs.js");
--- src/handlers/tools/peers.js
+++
@@ -1,9 +0,0 @@
-const { getClient } = require("../../util/clients.js");
-
-async function peersHandler(request, h) {
-  const client = getClient();
-  const peers = await client.execute("getpeerinfo");
-  return h.view("peers.pug", { peers });
-}
-
-module.exports = peersHandler;
