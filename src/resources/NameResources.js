925a9f3ce34f086ef119aec7de0067c2a1cf13c0
kilpatty <seanpkilgarriff@gmail.com>
2019-09-22 18:07:44 -0500

    resources: initial

---
+++ src/resources/NameResources.js
@@ -0,0 +1,26 @@
+import { Resource } from "rest-hooks";
+
+export default class NameResource extends Resource {
+  name = "";
+  start = null; //@todo not sure best type here.
+  reserved = false;
+  state = "";
+  nextState = "";
+  blocksUntilNextState = "";
+  nameHash = "";
+  height = null;
+  renewal = null;
+  value = null;
+  highest = null;
+  weak = false;
+  transfer = null;
+  revoked = null;
+  records = null; //@todo ideally vet this out further.
+  history = null; //@todo vet this out futher into types. This contains transactions, we can be cached.
+
+  pk() {
+    return this.name;
+  }
+
+  static urlRoot = "http://localhost/names/";
+}
