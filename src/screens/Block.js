42f2a95be9a124022103525f6ba6b1843ae72642
kilpatty <seanpkilgarriff@gmail.com>
2019-09-29 10:46:55 -0500

    screens: clean up block page

--- src/screens/Block.js
+++ src/screens/Block.js
@@ -9,10 +9,9 @@ import BlockResource from "../resources/BlockResource";
 import TransactionResource from "../resources/TransactionResource";
 import TransactionList from "../components/TransactionList";
 import BlockSummary from "../components/BlockSummary";
+import BlockAdvanced from "../components/BlockAdvanced";
 import Pagination from "../components/Pagination";
 import queryString from "query-string";
-
-import { timeAgo, sciNotation, hnsValues, checkPool } from "../util/util";
 
 function BlockSkeleton() {
   return (
@@ -25,124 +24,25 @@ function BlockSkeleton() {
 
 //@todo move most of this into a component, not in here.
 function BlockContainer({ height, page, changePage, url }) {
+  const limit = 10;
   const block = useResource(BlockResource.detailShape(), {
     height
   });
-  //@todo make this better
-  const limit = 10;
-  const offset = (page - 1) * limit;
   const txs = useResource(TransactionResource.listShape(), {
     height,
-    limit,
-    offset
+    page
   });
-  //@todo remove limit from the api, it's useless. Also remove offset.
   const { total } = useResultCache(TransactionResource.listShape(), {
     height,
-    limit,
-    offset
+    page
   });
   const pages = Math.ceil(total / limit);
-
-  let [difficulty, exponent] = sciNotation(block.difficulty, 5);
 
   return (
     // @todo should not come from home here
     <Home.ContentContainer>
       <BlockSummary block={block} />
-
-      {/* ------- Bottom Card ------ */}
-      <Cards.Card>
-        <Cards.Header>
-          <Cards.HeaderTitle>Advanced</Cards.HeaderTitle>
-        </Cards.Header>
-        {/* @todo remove all these class names. */}
-        {/* @todo need links in here */}
-        {/* @todo need auxilary labels -> bytes for size, scientific format for diff, etc */}
-        <div className="card-content">
-          <div className="columns">
-            <div className="column is-half">
-              <table className="table is-fullwidth">
-                <tbody>
-                  {block.prevBlock && (
-                    <tr>
-                      <StackedData
-                        label="Previous Block"
-                        value={block.prevBlock}
-                        link={"/block/" + (block.height - 1)}
-                      />
-                    </tr>
-                  )}
-                  <tr>
-                    <StackedData
-                      label="Difficulty"
-                      value={
-                        <span>
-                          {difficulty} x 10<sup>{exponent}</sup>
-                        </span>
-                      }
-                    />
-                  </tr>
-                  <tr>
-                    <StackedData label="Version" value={block.version} />
-                  </tr>
-                  <tr>
-                    <StackedData label="Bits" value={block.bits} />
-                  </tr>
-                  <tr>
-                    <StackedData label="Size" value={block.size + " bytes"} />
-                  </tr>
-                  <tr>
-                    <StackedData
-                      label="Average Fee"
-                      value={hnsValues(block.averageFee)}
-                    />
-                  </tr>
-                  <tr>
-                    <StackedData label="Nonce" value={block.nonce} />
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className="column is-half">
-              <table className="table is-fullwidth">
-                <tbody>
-                  {block.nextHash && (
-                    <tr>
-                      <StackedData
-                        label="Next Block"
-                        value={block.nextHash}
-                        link={"/block/" + (block.height + 1)}
-                      />
-                    </tr>
-                  )}
-                  <tr>
-                    <StackedData label="Hash" value={block.hash} />
-                  </tr>
-                  <tr>
-                    <StackedData label="Merkle Root" value={block.merkleRoot} />
-                  </tr>
-                  <tr>
-                    <StackedData label="Tree Root" value={block.treeRoot} />
-                  </tr>
-                  <tr>
-                    <StackedData label="Filter Root" value={block.filterRoot} />
-                  </tr>
-                  <tr>
-                    <StackedData
-                      label="Reserved Root"
-                      value={block.reservedRoot}
-                    />
-                  </tr>
-                  <tr>
-                    <StackedData label="Chainwork" value={block.chainwork} />
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </Cards.Card>
+      <BlockAdvanced block={block} />
       <TransactionList txs={txs} />
       <Pagination
         totalPages={pages}
