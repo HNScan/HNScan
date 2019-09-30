4af13a5100362bcc5fe793f09976f05431b78ff8
kilpatty <seanpkilgarriff@gmail.com>
2019-09-30 13:38:06 -0500

    search: add general container component, implement in search and clean up

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -2,22 +2,7 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import styled from "styled-components";
 import SearchBar from "../components/SearchBar";
-
-//@todo this needs to be fixed, and exported as a component for ALL screens
-const Wrapper = styled.div`
-  margin: 50px auto 60px;
-  text-align: center;
-  width: 95%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  min-height: calc(100vh - 460px);
-
-  @media (min-width: 1024px) {
-    width: 90%;
-    max-width: 1216px;
-  }
-`;
+import ContentContainer from "../components/ContentContainer";
 
 const Header = styled.h1`
   margin-top: 50px;
@@ -36,35 +21,29 @@ const SearchResultsTable = styled.table`
   font-size: 20px;
 `;
 
-const TableCell = styled.td`
-  color: ${props => props.theme["--text-color-normal"]};
-`;
-
 const SearchBarWrapper = styled.div`
   margin-top: 50px;
 `;
 
-//@todo we need a content wrapper here.
-//@todo probably need to wrap most pages with that.
 export default function Search() {
   const location = useLocation();
   console.log(location);
 
   if (location.state.length === 0) {
     return (
-      <Wrapper>
+      <ContentContainer>
         <Header>We couldn't find what you were looking for</Header>
         <h2>Please try searching again</h2>
         <SearchBarWrapper>
           <SearchBar />
         </SearchBarWrapper>
-      </Wrapper>
+      </ContentContainer>
     );
   }
 
   const results = location.state.map((result, index) => (
     <tr key={index}>
-      <TableCell>{result.type}:</TableCell>
+      <td>{result.type}:</td>
       <td>
         <Link to={result.url}>{result.url}</Link>
       </td>
@@ -72,11 +51,11 @@ export default function Search() {
   ));
 
   return (
-    <Wrapper>
+    <ContentContainer>
       <Header>We found multiple resources matching your search</Header>
       <SearchResultsTable className="table">
         <tbody>{results}</tbody>
       </SearchResultsTable>
-    </Wrapper>
+    </ContentContainer>
   );
 }
