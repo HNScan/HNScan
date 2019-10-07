c017db14d2ad1be0e912d0c473134183e76e3f94
kilpatty <seanpkilgarriff@gmail.com>
2019-10-07 18:25:56 -0500

    names: convert to new page hooks

--- src/screens/Name.js
+++ src/screens/Name.js
@@ -1,15 +1,18 @@
-import React, { Suspense, useState, useEffect } from "react";
+import React, { Suspense } from "react";
 import { useParams } from "react-router-dom";
-import { useLocation, useHistory } from "react-router-dom";
-//@todo ideally get rid of this lib.
-import queryString from "query-string";
 import { useResource, useResultCache } from "rest-hooks";
-//@todo put all the name components into one folder in components.
+
+// Components
 import NameSummary from "../components/NameSummary";
-import NameResource from "../resources/NameResource";
-import NameHistoryResource from "../resources/NameHistoryResource";
 import NameAdvanced from "../components/NameAdvanced";
 import NameHistory from "../components/NameHistory";
+
+// Resources
+import NameHistoryResource from "../resources/NameHistoryResource";
+import NameResource from "../resources/NameResource";
+
+// Hooks
+import usePage from "../hooks/usePage";
 //@todo remove this.
 
 const NameRecords = () => <div>todo</div>;
@@ -17,6 +20,7 @@ const NameRecords = () => <div>todo</div>;
 function NameView({ name, page, changePage, url }) {
   //Run these in parallel
   const nameData = useResource(NameResource.detailShape(), { name });
+  //@todo move this to NameHistory component, since we want to be able to filter it effectively.
   const history = useResource(NameHistoryResource.listShape(), { name });
   const { limit, total } = useResultCache(NameHistoryResource.listShape(), {
     name
@@ -30,52 +34,20 @@ function NameView({ name, page, changePage, url }) {
       <NameHistory
         history={history}
         page={page}
-        changePage={changePage}
         pages={pages}
-        url={url}
+        url={"/name/" + name}
       />
     </>
   );
 }
 
 export default function Name() {
-  const location = useLocation();
-  let currentPage = 1;
-  let query = queryString.parse(location.search);
-  // let currentPage = 1;
-  if (!isNaN(parseInt(query.p)) && query.p > 0) {
-    currentPage = parseInt(query.p);
-  }
-
-  const [page, setPage] = useState(currentPage);
-  useEffect(() => {
-    let currentPage = 1;
-    let query = queryString.parse(location.search);
-    // let currentPage = 1;
-    if (!isNaN(parseInt(query.p)) && query.p > 0) {
-      currentPage = parseInt(query.p);
-    }
-    setPage(currentPage);
-  }, [location.search]);
-  const history = useHistory();
-
-  //@todo this is actually probably bad. Don't pass a state changer down, let's just have Pagination use Link components.
-  const changePage = page => {
-    // Update Page Location
-    // I wonder if this will work...
-    history.push({ search: "?p=" + page });
-    setPage(page);
-  };
+  const page = usePage();
   const { name } = useParams();
   return (
     <>
       <Suspense fallback={<div>Loading...</div>}>
-        <NameView
-          name={name}
-          page={page}
-          changePage={changePage}
-          url={"/name/" + name}
-        />
+        <NameView name={name} page={page} />
       </Suspense>
     </>
   );
