6dd66359bdec2fe3bdcad8bc3db48aaa02f87cee
Shane Jann <shanejann@gmail.com>
2019-10-22 16:22:28 -0500

    refactor: move block and name table-related components into their own component files

--- src/screens/Blocks.js
+++ src/screens/Blocks.js
@@ -1,155 +1,33 @@
 import React, { Suspense } from "react";
+
+// Resources
 import { useResource, useResultCache } from "rest-hooks";
-
-// Components
-import Pagination from "components/layout/Pagination";
-import Card from "components/styles/Card";
-import styled from "styled-components";
-import { Link } from "react-router-dom";
-import Table from "reactbulma/lib/components/Table/Table.js";
-import Skeleton from "react-loading-skeleton";
-
-// Util
-import { timeAgo, truncateHash } from "utils/util";
+import BlockResource from "resources/BlockResource";
 
 // Hooks
 import usePage from "hooks/usePage";
 
-// Resources
-import BlockResource from "resources/BlockResource";
+// Components
+import { BlocksTable, BlocksSkeleton } from "components/block/BlocksTable";
 
-const TableContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  padding: 0;
-  @media (min-width: 680px) {
-    padding: 1.5rem;
-  }
-`;
-
-const DataTable = styled(Table)`
-  width: 90%;
-  height: auto;
-  margin: 10px auto;
-  @media (min-width: 680px) {
-    width: 95%;
-  }
-`;
-
-function BlocksSkeleton() {
-  const rows = [];
-  for (let i = 0; i < 24; i++)
-    rows.push(<Row key={i} loading />)
-  return rows;
-}
-
-function BlocksTable({ page }) {
+function BlocksView({ page }) {
   const pageOffset = (page - 1) * 25;
   const blocks = useResource(BlockResource.listShape(), { offset: pageOffset });
   const { limit, total } = useResultCache(BlockResource.listShape(), {
     offset: pageOffset
   });
   const pages = Math.ceil(total / limit);
-  const blockRows = blocks.map((block) => (
-    <Row key={block.height}
-      height={block.height}
-      size={block.size}
-      time={block.time}
-      miner={block.miner}
-      txs={block.txs} />
-  ));
-  return (
-    <>
-      <BlocksTableStructure>
-        {blockRows}
-      </BlocksTableStructure>
-      <Pagination totalPages={pages} page={page} url="/blocks" />
-    </>
-  );
+
+  return <BlocksTable blocks={blocks} pages={pages} page={page} />
 }
 
-function BlocksTableStructure({ children }) {
-  return (
-    <Card>
-      <Card.Header>
-        <Card.HeaderTitle>HNS Blocks</Card.HeaderTitle>
-      </Card.Header>
-      <Card.Content>
-        <TableContainer>
-          <DataTable>
-            <Table.Head>
-              <Table.Tr>
-                <Table.Th>
-                  <abbr title="Block Height">Height</abbr>
-                </Table.Th>
-                <Table.Th className="is-hidden-mobile">
-                  <abbr title="Block Age">Age</abbr>
-                </Table.Th>
-                <Table.Th>
-                  <abbr title="Miner Address">Miner</abbr>
-                </Table.Th>
-                <Table.Th className="is-hidden-mobile">
-                  <abbr title="Block Size">Size</abbr>
-                </Table.Th>
-                <Table.Th>
-                  <abbr title="Number of Transactions">TXs</abbr>
-                </Table.Th>
-              </Table.Tr>
-            </Table.Head>
-            <Table.Body>{children}</Table.Body>
-          </DataTable>
-        </TableContainer>
-      </Card.Content>
-    </Card>
-  )
-}
-
-const Row = ({ height, size, time, miner, txs, loading }) => {
-  if (loading) {
-    // @todo figure out a more elegant way to construct the skeleton
-    return (
-      <Table.Tr>
-        <Table.Td className="is-hidden-mobile" width="10%"><Skeleton /></Table.Td>
-        <Table.Td><Skeleton /></Table.Td>
-        <Table.Td width="50%"><Skeleton /></Table.Td>
-        <Table.Td width="10%"><Skeleton /></Table.Td>
-        <Table.Td className="is-hidden-mobile" width="10%"><Skeleton /></Table.Td>
-      </Table.Tr>
-    );
-  }
-  return (
-    <Table.Tr>
-      <Table.Td>
-        <Link to={"/block/" + height}>{height}</Link>
-        <div className="is-hidden-tablet">Size: {size}</div>
-      </Table.Td>
-      <Table.Td className="is-hidden-mobile">{timeAgo(time)}</Table.Td>
-      <Table.Td>
-        <Link className="is-hidden-mobile" to={"/address/" + miner}>
-          {miner}
-        </Link>
-        <Link className="is-hidden-tablet" to={"/address/" + miner}>
-          {truncateHash(miner)}
-        </Link>
-        <div className="is-hidden-tablet">{timeAgo(time)}</div>
-      </Table.Td>
-      <Table.Td className="is-hidden-mobile">{size}</Table.Td>
-      <Table.Td>{txs}</Table.Td>
-    </Table.Tr>
-  )
-}
-
-export default function Blocks(props) {
+export default function Blocks() {
   const page = usePage();
 
   return (
     <>
-      <Suspense fallback={
-        <BlocksTableStructure>
-          <BlocksSkeleton />
-        </BlocksTableStructure>
-      }>
-        <BlocksTable page={page} />
+      <Suspense fallback={<BlocksSkeleton />}>
+        <BlocksView page={page} />
       </Suspense>
     </>
   );
--- src/screens/Names.js
+++ src/screens/Names.js
@@ -1,118 +1,27 @@
 import React, { Suspense, useState, useEffect } from "react";
-import { Link, useLocation, useHistory } from "react-router-dom";
-import Table from "reactbulma/lib/components/Table/Table.js";
-import styled from "styled-components";
-import queryString from "query-string";
 import { useResource, useResultCache } from "rest-hooks";
+import { useLocation, useHistory } from "react-router-dom";
+import queryString from "query-string";
 
 // Components
-import Card from "components/styles/Card";
-import Pagination from "components/layout/Pagination";
-import Skeleton from "react-loading-skeleton";
+import { NamesSkeleton, NamesTable } from "components/name/NameList";
 
 // Resources
 import NameResource from "resources/NameResource";
 
-const TableContainer = styled.div`
-  height: 100%;
-  width: 100%;
-  padding: 0;
-  @media (min-width: 680px) {
-    padding: 1.5rem;
-  }
-`;
+//Turn getPage into it's own hook.... @todo
 
-const DataTable = styled(Table)`
-  width: 90%;
-  height: auto;
-  margin: 10px auto;
-  @media (min-width: 680px) {
-    width: 95%;
-  }
-`;
-
-const Row = ({ name, state, height, loading }) => (
-  <Table.Tr>
-    <Table.Td width="75%">
-      {loading ? <Skeleton /> : <Link to={"/name/" + name}>{name}</Link>}
-    </Table.Td>
-    <Table.Td>
-      {loading ? <Skeleton /> : state}
-      </Table.Td>
-    <Table.Td>
-      {loading ? <Skeleton /> : <Link to={"/block/" + height}>{height}</Link>}
-    </Table.Td>
-  </Table.Tr>
-);
-
-function NamesTableStructure({ children }) {
-  return (
-    <Card>
-      <Card.Header>
-        <Card.HeaderTitle>TLD Names</Card.HeaderTitle>
-      </Card.Header>
-      <Card.Content>
-        <TableContainer>
-          <DataTable>
-            <Table.Head>
-              <Table.Tr>
-                <Table.Th>
-                  <abbr title="Top Level Domain Name">Name</abbr>
-                </Table.Th>
-                <Table.Th>
-                  <abbr title="Name Auction State">State</abbr>
-                </Table.Th>
-                <Table.Th>
-                  <abbr title="Block Height">Height</abbr>
-                </Table.Th>
-              </Table.Tr>
-            </Table.Head>
-            <Table.Body>{children}</Table.Body>
-          </DataTable>
-        </TableContainer>
-      </Card.Content>
-    </Card>
-  );
-}
-
-function NamesSkeleton() {
-  const rows = [];
-  for (let i = 0; i < 24; i++)
-    rows.push(<Row key={i} loading />)
-  return rows;
-}
-
-function NamesTable({ page, changePage }) {
+function NamesView({ page, changePage }) {
   const pageOffset = (page - 1) * 25;
   const names = useResource(NameResource.listShape(), { offset: pageOffset });
   const { limit, total } = useResultCache(NameResource.listShape(), {
     offset: pageOffset
   });
   const pages = Math.ceil(total / limit);
-  const nameRows = names.map((name, index) => (
-    <Row key={index} name={name.name} state={name.state} height={name.height} />
-  ));
-
-  // 25 blocks per page
-  return (
-    <>
-      <NamesTableStructure>
-        {nameRows}
-      </NamesTableStructure>
-      <Pagination
-        totalPages={pages}
-        page={page}
-        url="/names"
-        changePage={changePage}
-      />
-    </>
-  );
+  return <NamesTable names={names} page={page} pages={pages} changePage={changePage} />
 }
 
-//Turn getPage into it's own hook.... @todo
-
-//@todo back button is not working right now.
-export default function Names(props) {
+export default function Names() {
   const location = useLocation();
   let currentPage = 1;
   let query = queryString.parse(location.search);
@@ -142,14 +51,8 @@ export default function Names(props) {
 
   return (
     <>
-      <Suspense
-        fallback={
-          <NamesTableStructure>
-            <NamesSkeleton />
-          </NamesTableStructure>
-        }
-      >
-        <NamesTable page={page} changePage={changePage} />
+      <Suspense fallback={<NamesSkeleton />}>
+        <NamesView page={page} changePage={changePage} />
       </Suspense>
     </>
   );
