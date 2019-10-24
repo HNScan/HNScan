85554f137e2361f5dee9f9f186873c153d8dc705
kilpatty <seanpkilgarriff@gmail.com>
2019-10-24 11:12:27 -0500

    router: scroll to top on search params changing

--- src/components/shared/ScrollToTop.js
+++ src/components/shared/ScrollToTop.js
@@ -2,11 +2,11 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [pathname]);
+  }, [pathname, search]);
 
   return null;
 }
