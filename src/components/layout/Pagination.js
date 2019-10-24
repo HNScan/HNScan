cd877cee9c88ff4bb5ffba58b0737d7b6b86dca9
kilpatty <seanpkilgarriff@gmail.com>
2019-10-24 11:11:21 -0500

    Revert "pagination: scroll to top on remount"
    This reverts commit 6be779ea7c9aaf720c6d1e29d6579522f68338c1.

--- src/components/layout/Pagination.js
+++ src/components/layout/Pagination.js
@@ -2,9 +2,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-// Components
-import ScrollToTopOnMount from "components/shared/ScrollToTopOnMount";
-
 //@todo move completely to styled components (Pagination Nav, is-centered).
 
 const Wrapper = styled.nav`
@@ -143,7 +140,6 @@ export default function Pagination({ totalPages, page, url }) {
       role="navigation"
       aria-label="pagination"
     >
-      <ScrollToTopOnMount />
       <Previous page={page - 1} url={url} />
       <Next page={page} lastPage={totalPages} url={url} />
       <List className="pagination-list">
--- src/components/shared/ScrollToTopOnMount.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-
-export default function ScrollToTopOnMount() {
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  return null;
-}
