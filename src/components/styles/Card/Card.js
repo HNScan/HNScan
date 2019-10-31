d58473af43a92f076afdd46ccea09cd972dfcd92
Shane Jann <shanejann@gmail.com>
2019-10-31 15:42:10 -0500

    feat: fix Transaction, AddressSummary, remove additional Card.js containers

--- src/components/styles/Card/Card.js
+++ src/components/styles/Card/Card.js
@@ -1,51 +1,7 @@
 // These are custom components for the home screen
 import styled from "styled-components";
 
-// ----- REUSABLE GENERICS -----
-export const Card = styled.div`
-  overflow: hidden;
-  border-radius: 10px;
-  width: 100%;
-  height: 100%;
-  background-color: ${props => props.theme["--card-background"]};
-  color: ${props => props.theme["--text-color-normal"]};
-  box-shadow: 0 2px 3px rgba(10, 10, 10, 0.1), 0 0 0 1px rgba(10, 10, 10, 0.1);
-
-  & a:hover {
-    color: ${props => props.theme["--text-color-normal"]};
-  }
-
-  &:not(:first-child) {
-    margin-top: 20px;
-  }
-`;
-
-// The header of every card
-const Header = styled.div`
-  align-items: stretch;
-  box-shadow: 0 1px 2px rgba(10, 10, 10, 0.1);
-  display: flex;
-`;
-
-// The header title on every card
-const HeaderTitle = styled.div`
-  // color: ${props => props.theme["--text-color-normal"]};
-  align-items: center;
-  display: flex;
-  flex-grow: 1;
-  font-weight: 700;
-  padding: 0.75rem;
-`;
-
-// Content container for all cards
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: auto;
-  padding: 1.5rem;
-`;
-
-// ----- TX'S AND BLOCKS SPECIFIC -----
+export const Card = {};
 
 const SummaryItem = styled.div`
   display: flex;
@@ -89,9 +45,6 @@ const ItemLogo = styled.div`
   margin: 1px 5px 0 0;
 `;
 
-Card.Header = Header;
-Card.HeaderTitle = HeaderTitle;
-Card.Content = Content;
 Card.SummaryItem = SummaryItem;
 Card.LeftItemDetail = LeftItemDetail;
 Card.RightItemDetail = RightItemDetail;
--- src/screens/Transaction.js
+++ src/screens/Transaction.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { Row, Col } from "@urkellabs/ucl";
 
 // Components
-import Card from "components/styles/Card";
+import { Card } from "@urkellabs/ucl";
 import StackedData from "components/shared/StackedData";
 import { InputList, OutputList } from "components/shared/PutsList";
 
@@ -38,96 +38,81 @@ function TxDetailScreen({ hash }) {
   return (
     <>
       {/* ------- Top Card ------ */}
-      <Card>
-        <Card.Header>
-          <Card.HeaderTitle>Transaction Summary</Card.HeaderTitle>
-        </Card.Header>
-        <Card.Content>
-          <Row>
-            <Col mobile={12} tablet>
-              <StackedData label="Received" value={timeAgo(tx.time)} />
-            </Col>
-            <Col mobile={12} tablet>
-              <StackedData label="Amount" value={hnsValues(sumTxOutputs(tx.outputs))} />
-            </Col>
-            <Col mobile={12} tablet>
-              <StackedData label="Fee" value={hnsValues(tx.fee)} />
-            </Col>
-            <Col mobile={12} tablet>
-              <StackedData label="Confirmation" value={tx.confirmations} />
-            </Col>
-          </Row>
-        </Card.Content>
+      <Card title="Transaction Summary">
+        <Row>
+          <Col mobile={12} tablet>
+            <StackedData label="Received" value={timeAgo(tx.time)} />
+          </Col>
+          <Col mobile={12} tablet>
+            <StackedData label="Amount" value={hnsValues(sumTxOutputs(tx.outputs))} />
+          </Col>
+          <Col mobile={12} tablet>
+            <StackedData label="Fee" value={hnsValues(tx.fee)} />
+          </Col>
+          <Col mobile={12} tablet>
+            <StackedData label="Confirmation" value={tx.confirmations} />
+          </Col>
+        </Row>
       </Card>
 
       {/* Bottom Card */}
-      <Card>
-        <Card.Header>
-          <Card.HeaderTitle>Advanced</Card.HeaderTitle>
-        </Card.Header>
-        <Card.Content>
-          <div className="columns">
-            <div className="column is-half">
-              <table className="table is-fullwidth">
-                <tbody>
-                  {/* TODO: Get Node Status */}
-                  <tr>
-                    <StackedData cell label="Hash" value={tx.hash} />
-                  </tr>
-                  <tr>
-                    <StackedData
-                      cell
-                      label="Block Height"
-                      value={tx.height}
-                      link={"/block/" + tx.height}
-                    />
-                  </tr>
-                  <tr>
-                    <StackedData cell label="Locktime" value={tx.locktime} />
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div className="column is-half">
-              <table className="table is-fullwidth">
-                <tbody>
-                  {/* TODO: Get Node Status */}
-                  <tr>
-                    <StackedData
-                      cell
-                      label="Witness Hash"
-                      value={tx.witnessHash}
-                    />
-                  </tr>
-                  <tr>
-                    <StackedData cell label="Version" value={tx.version} />
-                  </tr>
-                  <tr>
-                    <StackedData cell label="Index" value={tx.index} />
-                  </tr>
-                </tbody>
-              </table>
-            </div>
+      <Card title="Advanced">
+        <div className="columns">
+          <div className="column is-half">
+            <table className="table is-fullwidth">
+              <tbody>
+                {/* TODO: Get Node Status */}
+                <tr>
+                  <StackedData cell label="Hash" value={tx.hash} />
+                </tr>
+                <tr>
+                  <StackedData
+                    cell
+                    label="Block Height"
+                    value={tx.height}
+                    link={"/block/" + tx.height}
+                  />
+                </tr>
+                <tr>
+                  <StackedData cell label="Locktime" value={tx.locktime} />
+                </tr>
+              </tbody>
+            </table>
           </div>
-        </Card.Content>
+          <div className="column is-half">
+            <table className="table is-fullwidth">
+              <tbody>
+                {/* TODO: Get Node Status */}
+                <tr>
+                  <StackedData
+                    cell
+                    label="Witness Hash"
+                    value={tx.witnessHash}
+                  />
+                </tr>
+                <tr>
+                  <StackedData cell label="Version" value={tx.version} />
+                </tr>
+                <tr>
+                  <StackedData cell label="Index" value={tx.index} />
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </Card>
 
-      <Card>
-        <Card.Header>
-          <Card.HeaderTitle>TX Activity</Card.HeaderTitle>
-        </Card.Header>
-        <Card.Content>
-          <Container>
-            <div className="columns">
-              <div className="column is-half">
-                <InputList inputs={tx.inputs} />
-              </div>
-              <div className="column is-half">
-                <OutputList outputs={tx.outputs} />
-              </div>
+      <Card title="TX Activity">
+        <Container>
+          <div className="columns">
+            <div className="column is-half">
+              <InputList inputs={tx.inputs} />
             </div>
-          </Container>
-        </Card.Content>
+            <div className="column is-half">
+              <OutputList outputs={tx.outputs} />
+            </div>
+          </div>
+        </Container>
       </Card>
     </>
   );
