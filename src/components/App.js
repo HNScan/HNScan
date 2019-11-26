562db9f7c94e9ff04d7c190812c372cb7c06ee9b
kilpatty <seanpkilgarriff@gmail.com>
2019-11-25 18:58:17 -0600

    misc: remove last bits of rest-hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { NetworkErrorBoundary } from "rest-hooks";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, useTheme, ApiConfig } from "@urkellabs/ucl";
 
@@ -45,45 +44,43 @@ function App() {
   const [theme] = useTheme();
   const [network] = useNetwork();
   return (
-    <NetworkErrorBoundary>
-      <ThemeProvider theme={theme}>
-        <ApiConfig config={{ url: network }}>
-          <>
-            <GlobalStyles />
-            <Router>
-              <ScrollToTop />
-              <Navigation />
-              <ContentContainer>
-                <Switch>
-                  <Route path="/" exact component={Home} />
-                  <Route path="/address/:hash" exact component={Address} />
-                  <Route path="/blocks" exact component={Blocks} />
-                  <Route path="/block/:height" exact component={Block} />
-                  <Route path="/names" exact component={Names} />
-                  <Route path="/name/:name" exact component={Name} />
-                  {/* Ideally let's get a recent transactions page going */}
-                  <Route path="/tx/:hash" exact component={Transaction} />
-                  <Route path="/search" exact component={Search} />
-                  {/* More Screens */}
-                  {/* Tool Screens */}
-                  <Route path="/peers" exact component={Peers} />
-                  <Route path="/status" exact component={NodeStatus} />
-                  <Route path="/charts" exact component={Charts} />
-                  <Route path="/changelog" exact component={Changelog} />
-                  <Route
-                    path="/airdropclaim"
-                    exact
-                    component={AirdropClaimScreen}
-                  />
-                  <Route path="*" component={NotFoundScreen} />
-                </Switch>
-              </ContentContainer>
-              <Footer />
-            </Router>
-          </>
-        </ApiConfig>
-      </ThemeProvider>
-    </NetworkErrorBoundary>
+    <ThemeProvider theme={theme}>
+      <ApiConfig config={{ url: network }}>
+        <>
+          <GlobalStyles />
+          <Router>
+            <ScrollToTop />
+            <Navigation />
+            <ContentContainer>
+              <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/address/:hash" exact component={Address} />
+                <Route path="/blocks" exact component={Blocks} />
+                <Route path="/block/:height" exact component={Block} />
+                <Route path="/names" exact component={Names} />
+                <Route path="/name/:name" exact component={Name} />
+                {/* Ideally let's get a recent transactions page going */}
+                <Route path="/tx/:hash" exact component={Transaction} />
+                <Route path="/search" exact component={Search} />
+                {/* More Screens */}
+                {/* Tool Screens */}
+                <Route path="/peers" exact component={Peers} />
+                <Route path="/status" exact component={NodeStatus} />
+                <Route path="/charts" exact component={Charts} />
+                <Route path="/changelog" exact component={Changelog} />
+                <Route
+                  path="/airdropclaim"
+                  exact
+                  component={AirdropClaimScreen}
+                />
+                <Route path="*" component={NotFoundScreen} />
+              </Switch>
+            </ContentContainer>
+            <Footer />
+          </Router>
+        </>
+      </ApiConfig>
+    </ThemeProvider>
   );
 }
 
