93393968e7d56ae1279b6e2e4796180c031b0070
kilpatty <seanpkilgarriff@gmail.com>
2019-11-25 18:56:30 -0600

    hooks: add in custom hook for useNetwork, save in persisted state

--- src/components/App.js
+++ src/components/App.js
@@ -37,13 +37,17 @@ import NotFoundScreen from "screens/errors/NotFound";
 // Internationalization
 import "../i18n/i18n";
 
+// Hooks
+import useNetwork from "hooks/useNetwork";
+
 //@todo useNetwork custom hook
 function App() {
   const [theme] = useTheme();
+  const [network] = useNetwork();
   return (
     <NetworkErrorBoundary>
       <ThemeProvider theme={theme}>
-        <ApiConfig config={{ url: "http://localhost:8080" }}>
+        <ApiConfig config={{ url: network }}>
           <>
             <GlobalStyles />
             <Router>
