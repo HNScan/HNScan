f74c98dd137bc0c9a558bbc3b302c44c8bfd946c
kilpatty <seanpkilgarriff@gmail.com>
2019-12-18 13:27:29 -0600

    charts: add total tx, total supply, and total burned.

---
+++ src/containers/charts/TotalBurned.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useQuery, Card, LineChart } from "@urkellabs/ucl";
+
+export default function TotalBurned() {
+  //@todo eventually we can make this configurable, but no need right now.
+  const { data } = useQuery("/charts/burned", {
+    startTime: Math.floor(Date.now() / 1000) - 1000 * 24 * 3600,
+    endTime: Math.floor(Date.now() / 1000)
+  });
+
+  return (
+    <Card fullHeight title="Total HNS Burned">
+      <LineChart
+        data={data}
+        title="Burned HNS"
+        tooltip="Total Burned HNS: [bold]{valueY}[\]"
+        fill
+      />
+    </Card>
+  );
+}
---
+++ src/containers/charts/TotalSupply.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useQuery, Card, LineChart } from "@urkellabs/ucl";
+
+export default function TotalSupply() {
+  //@todo eventually we can make this configurable, but no need right now.
+  const { data } = useQuery("/charts/supply", {
+    startTime: Math.floor(Date.now() / 1000) - 1000 * 24 * 3600,
+    endTime: Math.floor(Date.now() / 1000)
+  });
+
+  return (
+    <Card fullHeight title="Total HNS Supply">
+      <LineChart
+        data={data}
+        title="Circulating HNS"
+        tooltip="Total HNS: [bold]{valueY}[\]"
+        fill
+      />
+    </Card>
+  );
+}
