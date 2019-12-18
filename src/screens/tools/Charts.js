b43f4b167aa619e234099d400d7df26043ecd0cc
kilpatty <seanpkilgarriff@gmail.com>
2019-12-18 09:58:40 -0600

    charts: add dailyTX as a container, and finish up the multiple charts impl

--- src/screens/tools/Charts.js
+++ src/screens/tools/Charts.js
@@ -1,45 +1,46 @@
 import React, { Suspense } from "react";
-import { Switch, Route, useRouteMatch, Tabs, Tab } from "react-router-dom";
+import { Switch, Route, useRouteMatch, Redirect } from "react-router-dom";
 import styled from "styled-components";
-import { Flex } from "@urkellabs/ucl";
+import { Flex, Tabs, Tab } from "@urkellabs/ucl";
 
 // Containers
 import DailyDifficulty from "containers/charts/DailyDifficulty";
+import DailyTransactions from "containers/charts/DailyTransactions";
 
 const Wrapper = styled(Flex)`
   height: 60vh;
   width: 100%;
+  margin-top: 20px;
 `;
 
 //@todo we need to figure out a better color scheme here as well.
 //@todo would be cool to link "source" like Etherscan does, but link it to our API docs when we have them.
 //@todo right now we default to difficulty for the "url"/charts -> I think we can in the future make this a lander for charts, rather than just default to difficulty similar to etherscan. Allow people to pick it out..
-//@todo do we need the switch here?
 
 export default function Charts() {
-  let { path, url } = useRouteMatch();
+  let { url } = useRouteMatch();
   return (
     <>
       <Tabs>
         <Tab to={`${url}/difficulty`}>Difficulty</Tab>
         <Tab to={`${url}/dailytransactions`}>Daily TX</Tab>
       </Tabs>
-
       <Switch>
-        <Route path={path}>
+        <Route path={url + "/difficulty"}>
           <Wrapper align="center" columns>
             <Suspense fallback={<div>Loading...</div>}>
               <DailyDifficulty />
             </Suspense>
           </Wrapper>
         </Route>
-        <Route path={url + "/charts/dailytransactions"}>
+        <Route path={url + "/dailytransactions"}>
           <Wrapper align="center" columns>
             <Suspense fallback={<div>Loading...</div>}>
-              <DailyDifficulty />
+              <DailyTransactions />
             </Suspense>
           </Wrapper>
         </Route>
+        <Redirect from={url} to={`${url}/difficulty`} />
       </Switch>
     </>
   );
