e270d3ef6028328584d1c5cf8da9e043b81deb71
kilpatty <seanpkilgarriff@gmail.com>
2020-05-27 16:14:53 -0500

    home: clean up styled components and fix transaction endpoint

--- src/components/home/RecentTransactions.js
+++ src/components/home/RecentTransactions.js
@@ -1,59 +1,60 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-import { Card, Flex, Col } from "@urkellabs/ucl";
+import styled from "styled-components";
+import { Card, Flex, Col, Row, Spacer, Text, breakpoint } from "@urkellabs/ucl";
 import { useTranslation, Trans } from "react-i18next";
-
-// Components
-import {
-  SummaryItem,
-  LeftItemDetail,
-  RightItemDetail,
-  ItemLogo
-} from "./styled-components";
 
 // SVGs
 import Arrow from "components/svg/RightArrow";
 
 // Util
-import { truncateHash, timeAgo, hnsValues, sumTxOutputs } from "utils/util";
+import { truncateHash, timeAgo, hnsValues } from "utils/util";
+
+let ArrowIcon = styled(Arrow)`
+  height: 20px;
+  width: 20px;
+  margin-right: 5px;
+`;
+
+let Time = styled(Flex)`
+  ${breakpoint.tablet} {
+    justify-content: flex-end;
+  }
+`;
 
 const Transaction = ({ tx }) => {
   return (
-    // ----- TX Details -----
-    <Col>
-      <SummaryItem>
-        {/* ----- Left Side / Top Side ----- */}
-        <Col mobile={9}>
-          <LeftItemDetail>
-            <ItemLogo>
-              <Arrow />
-            </ItemLogo>
+    <Row>
+      <Spacer px={10} />
+      <Col mobile={12} tablet>
+        <Flex columns>
+          <Flex>
+            <ArrowIcon />
             <Trans
               i18nKey="home.tx_num"
               values={{ hash: truncateHash(tx.hash) }}
             >
               <Link to={"/tx/" + tx.hash}></Link>
             </Trans>
-          </LeftItemDetail>
-          <LeftItemDetail>
+          </Flex>
+          <Flex>
             <Trans
               i18nKey="home.amount"
-              values={{ amount: hnsValues(sumTxOutputs(tx.outputs)) }}
+              values={{ amount: hnsValues(tx.value) }}
             />
-          </LeftItemDetail>
-          <LeftItemDetail>
-            <Trans i18nKey="home.fee" values={{ fee: hnsValues(tx.fee) }} />
-          </LeftItemDetail>
-        </Col>
-        {/* ----- Right Side / Bottom Side ----- */}
-        <Col>
-          <RightItemDetail>
-            <em>{timeAgo(tx.time)}</em>
-          </RightItemDetail>
-        </Col>
-      </SummaryItem>
-    </Col>
+          </Flex>
+          <Trans i18nKey="home.fee" values={{ fee: hnsValues(tx.fee) }} />
+        </Flex>
+      </Col>
+      <Col mobile={12} tablet>
+        {/* @todo instead of putting time here, it would be cool to put the Transaction type. */}
+        <Time>
+          <Text small>{timeAgo(tx.time)}</Text>
+        </Time>
+      </Col>
+      <Spacer px={10} />
+    </Row>
   );
 };
 
@@ -61,11 +62,7 @@ export default function RecentTransactions({ txs }) {
   const txRows = txs.map((tx, index) => <Transaction key={index} tx={tx} />);
   const { t } = useTranslation();
 
-  return (
-    <Card title={t("home.recent_transactions")}>
-      <Flex columns>{txRows}</Flex>
-    </Card>
-  );
+  return <Card title={t("home.recent_transactions")}>{txRows}</Card>;
 }
 
 RecentTransactions.propTypes = {
