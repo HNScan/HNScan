57d0ae53ee786af12c5e00291674514b6b9eec84
kilpatty <seanpkilgarriff@gmail.com>
2020-06-15 14:10:15 -0500

    misc: fix link in puts list

--- src/components/shared/PutsList.js
+++ src/components/shared/PutsList.js
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 import { Trans } from "react-i18next";
+
+import Link from "components/Link";
 
 // Util
 //@todo rename to toHNS()
